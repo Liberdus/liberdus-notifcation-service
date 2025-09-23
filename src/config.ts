9bcca1d8993ae1a7b63062e72b9dcee5e3e2d813
jairajdev <jairajranamagar.dev@gmail.com>
2025-09-23 14:53:42 +0545

    Standardize call ID generation and improve VoIP notification payload consistency

--- src/config.ts
+++ src/config.ts
@@ -37,7 +37,7 @@ const config: Config = {
     keyId: 'XXX',
     teamId: 'XXX',
     bundleId: 'XXX',
-    production: false,
+    production: true, // Make sure to set this to false in iOS development build
   },
 }
 
